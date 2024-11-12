4414427c38790642cb89b5130327d631142bad3e
Andrey Karetnikov <157799428+noychus@users.noreply.github.com>
2024-11-12 15:38:19 +0300

    start

---
+++ WishBoard/WishBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishBoardApp.swift
+//  WishBoard
+//
+//  Created by noychus on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WishBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
